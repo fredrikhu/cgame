b0f02b8f5d8d1b245ba1981d4da5647abbc6679d
Fredrik Hult <fredrik.hult@outlook.com>
2024-10-19 23:29:24 +0200

    Render multi colored triangle

--- src/main.c
+++ src/main.c
@@ -75,12 +75,10 @@ void process_input(GLFWwindow* window)
 void prepare_scene()
 {
 	float vertices[] = {
-		-1, -1, 0,
-		-1, 0, 0,
-		0, 0, 0,
-		0, -1, 0,
-		0, 0, 0,
-		1, 0, 0
+		// positions         // colors
+		 0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
+		-0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
+		 0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top
 	};
 	unsigned int indices[] = {
 		0, 1, 3,
@@ -98,8 +96,11 @@ void prepare_scene()
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
 	glBindVertexArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -166,15 +167,15 @@ void render_scene()
 	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	float time = glfwGetTime();
-	float green = (sin(time) / 2.0f) + 0.5f;
-	int vertex_color_location = glGetUniformLocation(shader_program, "ourColor");
+	//float time = glfwGetTime();
+	//float green = (sin(time) / 2.0f) + 0.5f;
+	//int vertex_color_location = glGetUniformLocation(shader_program, "ourColor");
 
 	glUseProgram(shader_program);
 
-	glUniform4f(vertex_color_location, 0.0f, green, 0.0f, 1.0f);
+	//glUniform4f(vertex_color_location, 0.0f, green, 0.0f, 1.0f);
 
 	glBindVertexArray(vao);
-	glDrawArrays(GL_TRIANGLES, 0, 6);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
 	glBindVertexArray(0);
 }
