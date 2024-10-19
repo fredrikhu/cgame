35cf324075d07dd9fa154d59e631bd83150ba764
Fredrik Hult <fredrik.hult@outlook.com>
2024-10-19 23:28:54 +0200

    Make sure read file content is null terminated

--- src/read_file.c
+++ src/read_file.c
@@ -7,12 +7,22 @@ char *read_file(char *path)
 	fopen_s(&file, path, "rb");
 
 	fseek(file, 0, SEEK_END);
-	int length = ftell(file);
+	size_t length = ftell(file);
 	fseek(file, 0, SEEK_SET);
 
-	char *buffer = malloc(length);
+	size_t read_bytes = 0;
+	char *buffer = malloc(length + 1);
 	if (buffer)
-		fread(buffer, 1, length, file);
+	{
+		buffer[length] = 0;
+		read_bytes = fread(buffer, 1, length, file);
+
+		if (read_bytes != length)
+		{
+			free(buffer);
+			return NULL;
+		}
+	}
 
 	fclose(file);
 
